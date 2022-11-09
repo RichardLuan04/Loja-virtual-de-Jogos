1278843ac8e7fbd1eedd2e94499b31a660984f54
r0hd <guilhermerohde44@gmail.com>
2022-11-08 21:55:28 -0300

    login  incompleto

--- js/login.js
+++ js/login.js
@@ -1,12 +1,20 @@
-function Logar() {
+async function fazerLogin() {
+    const email = document.getElementById("campoEmail")
+    const senha = document.getElementById("campoSenha")
 
-    const email = document.getElementById("")
+    const body = {
+        email: email.value,
+        senha: senha.value
+    }
 
+    const resposta = await fetch(" https://codifica-demo-api.herokuapp.com/api/v1/users/login", {
+        method: "POST",
+        body: JSON.stringify(body),
+        Headers: {
+            "const-type": "aplication/json"
+        }
+    })
 
-
-
-
-
-
-
+    const json = await resposta.json()
+    alert(json.mensagem)
 }
